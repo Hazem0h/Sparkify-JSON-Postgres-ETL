9b33e8975d2efdafff74a1a7489ac9d22a92ab16
HKhairy <s-hazem_khairy@zewailcity.edu.eg>
2022-11-27 14:33:32 +0200

    removed foreign key constraints. Will add them later after the exploratory stage

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -16,8 +16,8 @@ songplay_table_create = ("""
                              start_time FLOAT,
                              user_id VARCHAR references user_table(user_id),
                              level INT,
-                             song_id VARCHAR references song(song_id),
-                             artist_id VARCHAR references artist(artist_id),
+                             song_id VARCHAR,
+                             artist_id VARCHAR,
                              session_id INT,
                              location VARCHAR,
                              user_agent VARCHAR
@@ -49,7 +49,7 @@ song_table_create = ("""
                      CREATE TABLE IF NOT EXISTS song(
                          song_id VARCHAR PRIMARY KEY,
                          title VARCHAR,
-                         artist_id VARCHAR references artist(artist_id),
+                         artist_id VARCHAR,
                          year INT,
                          duration FLOAT
                      );
@@ -72,7 +72,7 @@ time_table_create = ("""
 
 songplay_table_insert = ("""
                          INSERT INTO songplay (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location VARCHAR, user_agent)
-                         VALUES (%s, %f, %s, %d, %s, %s, %s, %s, %s)
+                         VALUES (%s, %s, %s, %d, %s, %s, %s, %s, %s)
                          """)
 
 user_table_insert = ("""
@@ -80,9 +80,13 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+                     INSERT INTO song (song_id, title, artist_id, year, duration)
+                     VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+                       INSERT INTO artist(artist_id, name, location, latitude, longitude)
+                       VALUES(%s, %s, %s, %s, %s)
 """)
 
 
@@ -95,6 +99,15 @@ song_select = ("""
 """)
 
 # QUERY LISTS
+'''
+# Note
+Due to the foreign keys constraints, the tables need to be dropped in a certain order (the parent tables are dropped first, then the dependent tables)
+Otherwise, this will cause errors
 
+Similarly, in table creation, we can't make a foreign key point to a table that doesn't yet exist.
+
+Also, also, similarly in data insertion, we will have to fill the dimension tables first, before the fact table, since the fact table has foreign key
+constraints
+'''
 create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [user_table_drop, artist_table_drop, song_table_drop, time_table_drop, songplay_table_drop]
